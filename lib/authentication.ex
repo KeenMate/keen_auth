55ef1903e446a666ff2762a03080999196c5c355
Albert Moravec <albert.moravec@gmail.com>
2021-03-04 13:37:44 +0100

    Add redirect query parameter

--- lib/authentication.ex
+++ lib/authentication.ex
@@ -3,11 +3,11 @@ defmodule KeenAuth.Authentication do
     OpenIDConnect.authorization_uri(client, params)
   end
 
-  def logout_uri(client, redirect_to) do
+  def logout_uri(client, redirect_to, redirect_param \\ "post_logout_redirect_uri") do
     logout_endpoint =
       GenServer.call(:openid_connect, {:discovery_document, client})
       |> Map.get("end_session_endpoint")
 
-    logout_endpoint <> "?redirect_uri=" <> redirect_to
+    logout_endpoint <> "?" <> redirect_param <> "=" <> redirect_to
   end
 end
