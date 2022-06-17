3eb3943fb3de76ac9e0c1034a12fca0fcaac5ff4
Filip Jakab <jakabfilip.fj@gmail.com>
2022-06-17 20:38:30 +0200

    New base user mapping

--- lib/user.ex
+++ lib/user.ex
@@ -1,7 +1,9 @@
 defmodule KeenAuth.User do
+  alias Ecto.Changeset
+
   @keys [:id, :username, :display_name, :email]
 
-  @enforce_keys @keys
+  # @enforce_keys @keys
   defstruct @keys
 
   @type t() :: %__MODULE__{
@@ -10,4 +12,16 @@ defmodule KeenAuth.User do
     display_name: binary(),
     email: binary()
   }
+
+  @changeset_fields %{
+    id: :string,
+    username: :string,
+    display_name: :string,
+    email: :string
+  }
+
+  def new(params \\ %{}) do
+    {%__MODULE__{}, @changeset_fields}
+    |> Changeset.cast(params, @keys)
+  end
 end
