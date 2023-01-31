0fd525c0c3d311e36da9acb47b46893bb5fdba70
Filip Jakab <jakabfilip.fj@gmail.com>
2023-01-31 05:29:06 +0100

    Fixed function name typo

--- lib/authentication_controller.ex
+++ lib/authentication_controller.ex
@@ -70,7 +70,7 @@ defmodule KeenAuth.AuthenticationController do
     processor = Processor.current_processor(conn, provider)
 
     with user when not is_nil(user) <- storage.current_user(conn) do
-      processor.signout(conn, params)
+      processor.sign_out(conn, params)
     else
       nil ->
         RequestHelpers.redirect_back(conn, params)
